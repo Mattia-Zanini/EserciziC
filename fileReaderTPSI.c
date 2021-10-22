431fa1cac5340aafe7d877eca664e39cd2512f84
Mattia-Zanini <mattzanini2@gmail.com>
2021-10-22 16:25:52 +0100

    inizio lettura percorso file

--- fileReaderTPSI.c
+++ fileReaderTPSI.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]){
-    for(int i = 0; i < argc; i ++){
-        printf("%s\n", argv[i]);
+int main(int argc, char *argv[])
+{
+    if (strcmp(argv[3], "-c") == 0)
+    {
+        //cerca un carattere
+        printf("Cerco un carattere\n");
+        FILE *fp;
+        if((fp = fopen(argv[1], "r")) == NULL){
+            printf("File %s non trovato o impossibile da aprire\n", argv[1]);
+            int lenghtArray = 0;
+            lenghtArray = strlen(argv[0]);
+            printf("%s questo e' il percorso", argv[0]);
+            int toRemove = lenghtArray - 18;
+            exit(1);
+        }
+        else{
+            printf("File trovato e aperto");
+            fclose(fp);
+        }
     }
-    if(argv[3][0] == '-'){
-        printf("Salve");
+    else if (strcmp(argv[3], "-p") == 0)
+    {
+        //cerca una parola
+        printf("Cerco una parola\n");
     }
 }
