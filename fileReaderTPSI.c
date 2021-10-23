0df3a0ebae151031bbb15169738c6a4a5a499172
Mattia-Zanini <mattzanini2@gmail.com>
2021-10-23 17:26:15 +0200

    cerca correttamente il carattere, in una riga, però da problemi su più righe

--- fileReaderTPSI.c
+++ fileReaderTPSI.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int fsize(FILE *in_file);
-int searchCharacter(int fileSize, char *buff, char toFind);
+#define PATH_MAX 1000
+
+int searchCharacter(char *buff, char toFind, FILE *in_file);
 
 int main(int argc, char *argv[])
 {
@@ -22,39 +24,62 @@ int main(int argc, char *argv[])
         else
         {
             printf("File trovato e aperto\n");
-            int fileSize = fsize(in_file);
-            printf("Il file ha un quantitativo di caratteri pari a: %d\n", fileSize - 1); //meno l'EOF
-            char buff[fileSize];
-            fgets(buff, fileSize, in_file);
-            char toFind = argv[2][0];
-            int nChars = searchCharacter(fileSize, buff, toFind);
+            char toFind = argv[4][0];
+            char buff[256];
+            int nChars = searchCharacter(buff, toFind, in_file);
             printf("Nel file sono presenti: %d caratteri uguali a %c\n", nChars, toFind); //meno l'EOF
             fclose(in_file);
             printf("File chiuso\n");
+            //ottiene il percorso della cartella da dov eviene eseguito il codice
+            char dirPath[PATH_MAX];
+            if (getcwd(dirPath, sizeof(dirPath)) != NULL)
+            {
+                char file_OUT[FILENAME_MAX]; char pathSP[] = "/";
+                strcat(dirPath, pathSP); strcpy(file_OUT, argv[2]); strcat(dirPath, file_OUT);
+                FILE *out_file = fopen(dirPath, "w+"); // write + overwrite
+                fprintf(out_file, "Nel file sono state trovati %d caratteri che corrispondono a '%c'\n", nChars, argv[4][0]);
+                fclose(out_file);
+                printf("Percorso dove il risultato e' stato salvato: %s\n", dirPath);
+            }
+            else
+            {
+                perror("getcwd() error");
+                return 1;
+            }
         }
-        return 0;
     }
     else if (strcmp(argv[3], "-p") == 0)
     {
         //cerca una parola
         printf("Cerco una parola\n");
+        char *path = realpath(argv[1], NULL); //ottiene il percorso del file
+        FILE *in_file = fopen(path, "r");     // read only
+        free(path);
+        if (in_file == NULL)
+        {
+            printf("File %s non trovato o impossibile da aprire\n", argv[1]);
+            exit(1);
+        }
+        else
+        {
+        }
     }
+    return 0;
 }
-int fsize(FILE *in_file)
-{
-    int prev = ftell(in_file);
-    fseek(in_file, 0L, SEEK_END);
-    int sz = ftell(in_file);
-    fseek(in_file, prev, SEEK_SET); //go back to where we were
-    return sz;
-}
-int searchCharacter(int fileSize, char *buff, char toFind)
+int searchCharacter(char *buff, char toFind, FILE *in_file)
 {
     int nChar = 0;
-    for (int i = 0; i < fileSize - 1; i++)
+    while (fgets(buff, 256, in_file) != NULL)
     {
-        if(buff[i] == toFind)
-            nChar++;
+        printf("Questa e' la riga: %s\n", buff);
+        for (int i = 0; i < 256; i++)
+        {
+            if (buff[i] == toFind)
+            {
+                nChar++;
+                printf("Questo dovrebbere essere uguale %c\n", buff[i]);
+            }
+        }
     }
     return nChar;
 }
