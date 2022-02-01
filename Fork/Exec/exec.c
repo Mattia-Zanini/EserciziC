7f3bfbcb6ec45dd3a85ac1ad42fa23d4281f24d1
Mattia-Zanini <mattzanini2@gmail.com>
2022-02-01 14:37:42 +0100

    .

--- Fork/Exec/exec.c
+++ Fork/Exec/exec.c
@@ -11,10 +11,12 @@ int main()
     int p1 = fork();
     if (p1 == 0) //figlio 1
     {
+        printf("PID di p1: %d\n", getpid());
         close(1);
         dup(fd[1]);
         close(fd[1]);
         close(fd[0]);
+        //sleep(30);
         execl("/bin/cat", "cat", "file.txt", NULL);
         exit(0);
     }
@@ -23,16 +25,19 @@ int main()
         int p2 = fork();
         if (p2 == 0) //figlio 2
         {
+            printf("PID di p2: %d\n", getpid());
             close(0);
             dup(fd[0]);
             close(fd[0]);
             close(fd[1]);
             char cmd[] = "wc -l";
+            //sleep(30);
             execl("/bin/wc", cmd, NULL);
             exit(0);
         }
         else if (p2 > 0)
         {
+            printf("PID di p0: %d\n", getpid());
             wait(&status);
             close(fd[0]);
             close(fd[1]);
