486d01d0a5f5934288ab9a2e0819da007acb2a2d
Mattia-Zanini <mattzanini2@gmail.com>
2022-02-08 15:45:33 +0100

    Completato es con ricerca da parte di un figlio di una parola chiamando con la execl il comando 'grep'

---
+++ Fork/Exec/Es3/cercaStringhe.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define READ 0
+#define WRITE 1
+#define WORD_LENGTH 10
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2)
+    {
+        int status, tot = 0;
+        int piped[2];
+        int pStat = pipe(piped);
+        if (pStat == 0)
+        {
+            char str[WORD_LENGTH];
+            while (1)
+            {
+                printf("Inserisci la parola che vuoi cercare:\n");
+                scanf("%s", str);
+                if (strcmp(str, "fine") != 0)
+                {
+                    int p1 = fork();
+                    if (p1 == 0) // figlio
+                    {
+                        close(1);
+                        dup(piped[WRITE]);
+                        close(piped[WRITE]);
+                        close(piped[READ]);
+
+                        execl("/bin/grep", "grep", "-c", str, argv[1], NULL);
+                        exit(0);
+                    }
+                    else if (p1 > 0) // padre
+                    {
+                        wait(&status);
+                        char ris;
+                        read(piped[READ], &ris, 4);
+                        printf("'%s' è stata individuata %d volte\n\n", str, atoi(&ris));
+                        tot += atoi(&ris);
+                    }
+                    else // errore generazione figlio
+                    {
+                        printf("Errore durante la generazione del figlio\n");
+                        break;
+                    }
+                }
+                else // termina il ciclo
+                {
+                    break;
+                }
+            }
+            printf("La ricerca ha prodotto %d risultati totali\n", tot);
+        }
+        else
+        {
+            printf("Errore durante la creazione del canale pipe\n");
+        }
+    }
+    else
+    {
+        printf("Argomenti Errati\n");
+    }
+    return 0;
+}
